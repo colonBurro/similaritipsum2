57dbf4fcd7178127b95ec1e614c2accbec63032f
Hans <ian.hanser@protonmail.com>
2023-03-06 05:28:08 +0100

    add 2 api endpoints, one for learning documents, the other for classifying documents

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -34,4 +34,66 @@ class ApiController extends AbstractController
         $this->classifier = $classifier;
         $this->categoryRepository = $categoryRepository;
     }
+
+    #[Route('api/classifier/learn', name: 'classifier_learn')]
+    public function learn(Request $request)
+    {
+        $newDocuments = 0;
+        $invalidDocuments = 0;
+
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data["documents"]) || empty($data["documents"])) return $this->json(["documents"=>"invalid or missing 'documents' parameter."], Response::HTTP_BAD_REQUEST);
+        if (!isset($data["category"]) || empty($data["category"])) return $this->json(["message"=>"invalid or missing 'category' parameter."], Response::HTTP_BAD_REQUEST);
+
+        $category = $this->categoryRepository->findOneBy(["name" => $data["category"]]);
+        if (!$category) $category = new Category($data["category"]);
+
+        $this->classifier->checkAndSetCategory($data["category"])
+            ->setDocumentCount($category->getDocumentCount())
+            ->setTermCount($category->getTermCount())
+            ->setTermFrequencies($category->getTermFrequencies());
+
+        foreach ($data["documents"] as $document){
+            if($this->classifier->learn($data["category"], $document))$newDocuments++;
+            else $invalidDocuments++;
+        }
+
+        $classifierCategory = $this->classifier->fetchCategory($data["category"]);
+
+        $category->setDocumentCount($classifierCategory->getDocumentCount())
+            ->setTermCount($classifierCategory->getTermCount())
+            ->setTermFrequencies($classifierCategory->getTermFrequencies());
+
+        $this->categoryRepository->save($category, true);
+
+        return $this->json(["message" => "Succesfully added ". $newDocuments ." documents. ". $invalidDocuments ." documents were invalid."], Response::HTTP_OK);
+    }
+
+    #[Route('api/classifier/classify', name: 'classifier_classify')]
+    public function classify(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data["documents"]) || empty($data["documents"])) return $this->json(["documents"=>"invalid or missing 'documents' parameter."], Response::HTTP_BAD_REQUEST);
+
+        $categories = $this->categoryRepository->findAll();
+
+        $this->classifier->loadCategoriesFromDatabase($categories);
+        
+
+        $result = [];
+
+        foreach ($data["documents"] as $document){
+            $classifyResult = $this->classifier->classify($document);
+            $belongsTo = array_key_first($classifyResult);
+            $result[] = [
+                "document" => substr($document, 0, 30),
+                "probabilities" => $classifyResult,
+                "belongsTo" => $belongsTo
+            ];
+        }
+
+        return $this->json(["message" => $result], Response::HTTP_OK);
+    }
 }
